42ca573500b1ccf1d7e9d807d10de75c0013946b
Gert-Andry <gertandry@gmail.com>
2018-01-12 19:38:48 +0200

    Põhi tehtud

--- YT-Converter/Form1.Designer.cs
+++ YT-Converter/Form1.Designer.cs
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.linkBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.formatBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chooseDirectory = new System.Windows.Forms.Button();
+            this.checkKonsool = new System.Windows.Forms.CheckBox();
+            this.tõmba = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // linkBox
+            // 
+            this.linkBox.Location = new System.Drawing.Point(12, 28);
+            this.linkBox.Name = "linkBox";
+            this.linkBox.Size = new System.Drawing.Size(164, 20);
+            this.linkBox.TabIndex = 0;
+            this.linkBox.TextChanged += new System.EventHandler(this.linkBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sisesta YouTube link";
+            // 
+            // formatBox
+            // 
+            this.formatBox.FormattingEnabled = true;
+            this.formatBox.Location = new System.Drawing.Point(12, 76);
+            this.formatBox.Name = "formatBox";
+            this.formatBox.Size = new System.Drawing.Size(59, 21);
+            this.formatBox.TabIndex = 2;
+            this.formatBox.SelectedIndexChanged += new System.EventHandler(this.formatBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Vali formaat";
+            // 
+            // chooseDirectory
+            // 
+            this.chooseDirectory.Location = new System.Drawing.Point(191, 28);
+            this.chooseDirectory.Name = "chooseDirectory";
+            this.chooseDirectory.Size = new System.Drawing.Size(28, 20);
+            this.chooseDirectory.TabIndex = 4;
+            this.chooseDirectory.Text = "...";
+            this.chooseDirectory.UseVisualStyleBackColor = true;
+            this.chooseDirectory.Click += new System.EventHandler(this.chooseDirectory_Click);
+            // 
+            // checkKonsool
+            // 
+            this.checkKonsool.AutoSize = true;
+            this.checkKonsool.Location = new System.Drawing.Point(93, 80);
+            this.checkKonsool.Name = "checkKonsool";
+            this.checkKonsool.Size = new System.Drawing.Size(126, 17);
+            this.checkKonsool.TabIndex = 5;
+            this.checkKonsool.Text = "Tahan näha konsooli";
+            this.checkKonsool.UseVisualStyleBackColor = true;
+            this.checkKonsool.CheckedChanged += new System.EventHandler(this.checkKonsool_CheckedChanged);
+            // 
+            // tõmba
+            // 
+            this.tõmba.Location = new System.Drawing.Point(12, 114);
+            this.tõmba.Name = "tõmba";
+            this.tõmba.Size = new System.Drawing.Size(75, 23);
+            this.tõmba.TabIndex = 6;
+            this.tõmba.Text = "Tõmba";
+            this.tõmba.UseVisualStyleBackColor = true;
+            this.tõmba.Click += new System.EventHandler(this.tõmba_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 159);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(207, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(236, 200);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.tõmba);
+            this.Controls.Add(this.checkKonsool);
+            this.Controls.Add(this.chooseDirectory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.formatBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox linkBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox formatBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button chooseDirectory;
+        private System.Windows.Forms.CheckBox checkKonsool;
+        private System.Windows.Forms.Button tõmba;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- YT-Converter/Form1.cs
+++ YT-Converter/Form1.cs
@@ -7,14 +7,181 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
 
 namespace YT_Converter
 {
     public partial class Form1 : Form
     {
+        public string path;
+        public string link;
+        public string formaat;
+        public string kõik;
+
         public Form1()
         {
             InitializeComponent();
+            formatBox.Items.Add("m4a");
+            formatBox.Items.Add("mp3");
+            formatBox.Items.Add("wav");
+            formatBox.Items.Add("mp4@720p");
+            formatBox.Items.Add("mp4@360p");
+        }
+        private void linkBox_TextChanged(object sender, EventArgs e)
+        {
+            link = linkBox.Text;
+        }
+
+        private void formatBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chooseDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                path = fbd.SelectedPath;
+            }
+        }
+
+        private void checkKonsool_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tõmba_Click(object sender, EventArgs e)
+        {
+            int protsent = 0;
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                path = Directory.GetCurrentDirectory();
+            }
+
+            if (Convert.ToString(formatBox.SelectedItem) != "" || link != "")
+            {
+                formaat = Convert.ToString(formatBox.SelectedItem);
+                Process convert = new Process();
+                convert.StartInfo.FileName = "youtube-dl.exe";
+                SetStartInfo set = new SetStartInfo();
+                var failiNimi = set.SetArgument(convert, formaat, path, link);
+
+                if (!File.Exists(path + @"\" + failiNimi + "." + formaat) || !File.Exists(path + @"\" + failiNimi + ".mp4"))
+                {
+                    if (!checkKonsool.Checked)
+                    {
+                        convert.StartInfo.UseShellExecute = false;
+                        convert.StartInfo.CreateNoWindow = true;
+                        convert.StartInfo.RedirectStandardOutput = true;
+                        convert.Start();
+                        for (int a = 0; a < 6; a++)
+                        {
+                            kõik = convert.StandardOutput.ReadLine();
+                        }
+                        while (protsent != 100)
+                        {
+                            kõik = convert.StandardOutput.ReadLine();
+                            if (kõik != null)
+                            {
+                                string Protsenttekst = kõik.Substring(11, 6);
+                                Protsenttekst = Regex.Match(Protsenttekst, @"\d+").Value;
+                                try
+                                {
+                                    protsent = Int32.Parse(Protsenttekst);
+                                }
+                                catch (Exception)
+                                {
+                                    MessageBox.Show("See fail juba eksisteerib");
+                                    throw;
+                                }
+                                progressBar1.Value = protsent;
+                            }
+                            else
+                            {
+                                MessageBox.Show("Link ei tööta");
+                                break;
+                            }
+                        }
+                        convert.WaitForExit();
+                    }
+                    else
+                    {
+                        convert.Start();
+                        convert.WaitForExit();
+                    }
+                    if (File.Exists(path + @"\" + failiNimi + "." + formaat) || File.Exists(path + @"\" + failiNimi + ".mp4"))
+                    {
+                        MessageBox.Show("Fail lõpetas tõmbamise");
+                        progressBar1.Value = 0;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Miski on valesti");
+                        progressBar1.Value = 0;
+                    } 
+                }
+                else
+                {
+                    MessageBox.Show("See fail juba eksisteerib");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Palun täida kõik väljad");
+            }
+        }
+    }
+    class SetStartInfo
+    {
+        public string SetArgument(Process convert, string formaat, string path, string link)
+        {
+            var fileName = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "youtube-dl.exe",
+                    Arguments = "--get-filename " + link,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            fileName.Start();
+            string failiNimi = fileName.StandardOutput.ReadToEnd();
+            fileName.WaitForExit();
+            failiNimi = failiNimi.Replace("\n", "");
+
+            if (formaat == "mp3" || formaat == "m4a" || formaat == "wav")
+            {
+                convert.StartInfo.Arguments = "--extract-audio --audio-format " + formaat + " -o \"" + path + @"\" + failiNimi + "\"" + " " + link;
+            }
+            else
+            {
+                int indexNumber = failiNimi.IndexOf(".");
+                if (indexNumber > 0)
+                {
+                    failiNimi = failiNimi.Substring(0, indexNumber);
+                }
+                failiNimi = failiNimi + ".mp4";
+                if (formaat == "mp4@720p")
+                {
+                    convert.StartInfo.Arguments = "-f 22 -o \"" + path + @"\" + failiNimi + "\"" + " " + link;
+                }
+                else
+                {
+                    convert.StartInfo.Arguments = "-f 18 -o \"" + path + @"\" + failiNimi + "\"" + " " + link;
+                }
+            }
+            int indexNumber2 = failiNimi.IndexOf(".");
+            if (indexNumber2 > 0)
+            {
+                failiNimi = failiNimi.Substring(0, indexNumber2);
+            }
+            return failiNimi;
         }
     }
 }
